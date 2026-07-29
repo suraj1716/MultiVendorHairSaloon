6ae9f0cb55fb33bfa72ea22b37dda446b21c5940
suraj1716 <shrestha.suraj.2013@gmail.com>
2026-07-30 01:55:09 +1000

    build for Railway

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,6 @@
-# docker/entrypoint.sh
 #!/bin/sh
 set -e
 
-# Railway provides PORT; default to 8080 for local/dev use
 export PORT="${PORT:-8080}"
 
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
