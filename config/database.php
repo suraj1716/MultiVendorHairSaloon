0e12191261f82e3be1d54a0abf2c119af0530f0b
suraj1716 <43661849+suraj1716@users.noreply.github.com>
2026-07-31 14:59:16 +1000

    Remove SSL options from database.php
    Removed SSL options from database configuration.

--- config/database.php
+++ config/database.php
@@ -94,10 +94,7 @@ return [
     'prefix' => '',
     'prefix_indexes' => true,
     'search_path' => 'public',
-    'sslmode' => 'prefer',
-    'options' => [
-        PDO::ATTR_EMULATE_PREPARES => true,
-    ],
+    'sslmode' => 'prefer'
 ],
 
         'sqlsrv' => [
